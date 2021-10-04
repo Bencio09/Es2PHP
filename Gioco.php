7459793afae93fd1a70287d3b8484799baad98ab
Bencio09 <lorebencini13@outlook.com>
2021-10-04 18:33:55 +0200

    Creata funzione ControlloNumero.inc

---
+++ Gioco.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<head>
+    <?php 
+        include("./Functions/ControlloNumero.inc");
+    ?>
+    <link rel="stylesheet" href="./CSS/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="./CSS/MyStyle.css" type="text/css">
+    </head>
